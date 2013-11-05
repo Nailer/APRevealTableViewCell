220115e5fe198df97647685c87862ee02381451d
Andreas Petrov <andreas.petrov@soundrop.com>
2013-11-05 19:07:43 +0100

    Initial Commit

---
+++ APRevealTableViewCell/APViewController.h
@@ -0,0 +1,13 @@
+//
+//  APViewController.h
+//  APRevealTableViewCell
+//
+//  Created by Andreas Petrov on 05/11/13.
+//  Copyright (c) 2013 Andreas Petrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APViewController : UIViewController
+
+@end
---
+++ APRevealTableViewCellTests/APRevealTableViewCellTests.m
@@ -0,0 +1,34 @@
+//
+//  APRevealTableViewCellTests.m
+//  APRevealTableViewCellTests
+//
+//  Created by Andreas Petrov on 05/11/13.
+//  Copyright (c) 2013 Andreas Petrov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface APRevealTableViewCellTests : XCTestCase
+
+@end
+
+@implementation APRevealTableViewCellTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
