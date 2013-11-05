73e383e54ad61f961d1ed8196be559e2d741ff57
Andreas Petrov <andreas.petrov@soundrop.com>
2013-11-05 20:17:36 +0100

    Some initial groundwork in place

--- APRevealTableViewCell/APRevealTableViewCell.h
+++ APRevealTableViewCell/APRevealTableViewCell.h
@@ -8,9 +8,68 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol APRevealTableViewCellDelegate;
+
 @interface APRevealTableViewCell : UITableViewCell
 
-@property (weak, nonatomic) IBOutlet UIView *leftDrawerContent;
-@property (weak, nonatomic) IBOutlet UIView *rightDrawerContent;
+typedef enum
+{
+    APRevealTableViewCellSwipeDirectionNone = 0,
+    APRevealTableViewCellSwipeDirectionLeft = UISwipeGestureRecognizerDirectionLeft,
+    APRevealTableViewCellSwipeDirectionRight = UISwipeGestureRecognizerDirectionRight,
+} APRevealTableViewCellSwipeDirection;
+
+/**
+ The view to move when swiping the cell.
+ If nil, [self.contentView.subviews lastObject] is used
+ */
+@property (strong, nonatomic) IBOutlet UIView *mainContentView;
+
+/**
+ The view to display in the left drawer, which is revealed upon a swipe with APRevealTableViewCellSwipeDirectionRight
+ Set programmatically, or by connecting the outlet to a root-level view in a .xib
+ */
+@property (strong, nonatomic) IBOutlet UIView *leftDrawerContentView;
+
+/**
+ The view to display in the right drawer, which is revealed upon a swipe with APRevealTableViewCellSwipeDirectionLeft
+ Set programmatically, or by connecting the outlet to a root-level view in a .xib
+ */
+@property (strong, nonatomic) IBOutlet UIView *rightDrawerContentView;
+
+
+@property (weak, nonatomic) id <APRevealTableViewCellDelegate> delegate;
+
+@end
+
+@interface APRevealTableViewCell(SubclassingHooks)
+
+/**
+ Override in subclasses to react to changes in the reveal ratio.
+ Can be useful for adjusting the content in the drawers to the reveal ratio. For example fading in views by assigning revealRatio to alpha.
+ contentView is either leftDrawerContentView during a swipe to the right, or rightDrawerContentView during a swipe to the left
+ */
+- (void)revealRatioDidChange:(CGFloat)revealRatio forDrawerContentView:(UIView*)contentView;
+
+@end
+
+@protocol APRevealTableViewCellDelegate <NSObject>
+
+@optional
+
+/**
+ Asks the delegate if a swipe in the given direction should actually initiate a reveal
+ */
+- (BOOL)revealTableViewCell:(APRevealTableViewCell *)tableViewCell shouldRevealForSwipeDirection:(APRevealTableViewCellSwipeDirection)direction;
+
+/**
+ Called right before a reveal in the given direction is initiated
+ */
+- (void)revealTableViewCell:(APRevealTableViewCell *)tableViewCell willRevealForSwipeDirection:(APRevealTableViewCellSwipeDirection)direction;
+
+/**
+ Called right after a reveal in the given direction has finished
+ */
+- (void)revealTableViewCell:(APRevealTableViewCell *)tableViewCell didRevealForSwipeDirection:(APRevealTableViewCellSwipeDirection)direction;
 
 @end
--- APRevealTableViewCell/APRevealTableViewCell.m
+++ APRevealTableViewCell/APRevealTableViewCell.m
@@ -8,6 +8,130 @@
 
 #import "APRevealTableViewCell.h"
 
+@interface APRevealTableViewCell()
+
+@property (strong, nonatomic) UIPanGestureRecognizer *panGestureRecognizer;
+
+@property (nonatomic) APRevealTableViewCellSwipeDirection initialSwipeDirection;
+
+@end
+
 @implementation APRevealTableViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if ((self = [super initWithFrame:frame]))
+    {
+        [self setup];
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if ((self = [super initWithCoder:aDecoder]))
+    {
+        [self setup];
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    if ((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]))
+    {
+        [self setup];
+    }
+    
+    return self;
+}
+
+- (void)setup
+{
+    self.panGestureRecognizer = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(handleDidPan:)];
+}
+
+#pragma mark - Gesture handling
+
+- (void)handleDidPan:(UIPanGestureRecognizer *)recognizer
+{
+    CGPoint translation = [recognizer translationInView:self];
+
+    if (recognizer.state == UIGestureRecognizerStateBegan)
+    {
+        [self setSelected:NO];
+
+        BOOL isInitialPan = self.initialSwipeDirection == APRevealTableViewCellSwipeDirectionNone;
+        if (isInitialPan)
+        {
+            self.initialSwipeDirection = [self initialSwipeDirectionFromTranslation:translation];
+            [self setupViewsForPanInDirection:self.initialSwipeDirection];
+        }
+    }
+    else if (recognizer.state == UIGestureRecognizerStateChanged)
+    {
+
+
+    }
+    else if (recognizer.state == UIGestureRecognizerStateEnded ||
+             recognizer.state == UIGestureRecognizerStateCancelled)
+    {
+
+    }
+}
+
+- (void)setupViewsForPanInDirection:(APRevealTableViewCellSwipeDirection)direction
+{
+
+}
+
+- (APRevealTableViewCellSwipeDirection)initialSwipeDirectionFromTranslation:(CGPoint)translation
+{
+    if (translation.x > 0)
+    {
+        return APRevealTableViewCellSwipeDirectionRight;
+    }
+    else if (translation.x < 0)
+    {
+        return APRevealTableViewCellSwipeDirectionLeft;
+    }
+
+    return APRevealTableViewCellSwipeDirectionNone;
+}
+
+#pragma mark - Private
+
+- (BOOL)shouldRevealForSwipeDirection:(APRevealTableViewCellSwipeDirection)direction
+{
+    NSAssert(direction != APRevealTableViewCellSwipeDirectionNone, @"direction cannot be APRevealTableViewCellSwipeDirectionNone");
+    
+    UIView *content = [self drawerContentForSwipeDirection:direction];
+
+    return content != nil &&
+           [self.delegate respondsToSelector:@selector(revealTableViewCell:shouldRevealForSwipeDirection:)] &&
+           [self.delegate revealTableViewCell:self shouldRevealForSwipeDirection:direction];
+}
+
+- (UIView *)drawerContentForSwipeDirection:(APRevealTableViewCellSwipeDirection)direction
+{
+    NSAssert(direction != APRevealTableViewCellSwipeDirectionNone, @"direction cannot be APRevealTableViewCellSwipeDirectionNone");
+    return direction == APRevealTableViewCellSwipeDirectionLeft ? self.rightDrawerContentView : self.leftDrawerContentView;
+}
+
+- (UIView *)viewToPan
+{
+    return self.mainContentView ? self.mainContentView : [self.contentView.subviews lastObject];
+}
+
+@end
+
+@implementation APRevealTableViewCell(SubclassingHooks)
+
+- (void)revealRatioDidChange:(CGFloat)revealRatio forDrawerContentView:(UIView *)contentView
+{
+    // Default impl does nothing.
+}
 
 @end
